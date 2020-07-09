ee06d871ad22f1e17bf446076dfa87699ac2925a
Nayeem Ahmed <nayeemahmed24@gmail.com>
2020-07-10 00:04:35 +0600

    http used

---
+++ Model/Entities/MenuCategory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Model.Entities
+{
+    public class MenuCatergory
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [Required]
+        public string CategoryTitle { get; set; }
+        public MenuCatergory Parent { get; set; }
+        [Required]
+        public RestaurantModel Restaurant { get; set; }
+        [DefaultValue(false)]
+        public bool IsChildAvailable { get; set; }
+    }
+}
