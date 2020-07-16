bb904181c4aa3a607bc23ac1fa73190e34469d8c
Nayeem Ahmed <nayeemahmed24@gmail.com>
2020-07-17 00:38:02 +0600

    Order controller

--- Model/Entities/AddonUnit.cs
+++ Model/Entities/AddonUnit.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Model.Entities
 {
     public class AddonUnit
     {
+        [Required]
         public string AddonId { get; set; }
+        [Required]
         public double Quantity { get; set; }
     }
 }
--- Model/Entities/Order.cs
+++ Model/Entities/Order.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Model.Entities
 {
-    class Order
+    public class Order
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [Required]
+        public List<OrderUnit> Items { get; set; }
+        public OrderStatus Status { get; set; }
     }
 }
